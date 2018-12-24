784c1dbd4d4f11d9d2af027fa708f63c761d98b8
unknown <playbox8g@gmail.com>
2018-12-24 19:14:08 +0800

    initial commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://user200:abcd1234@ds243054.mlab.com:43054/todo-api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	mongoose
