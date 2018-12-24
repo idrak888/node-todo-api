d0323785bac15a4ddc16d03826af5cf32b8aaf0b
unknown <playbox8g@gmail.com>
2018-12-24 19:18:10 +0800

    initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
